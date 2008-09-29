d5233eccf09e53d3ead7e3666e65d449050f08ae
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2008-09-29 03:44:59 +0000

    Import of MATTN/Net-Kotonoha-0.08 from CPAN.
    gitpan-cpan-distribution: Net-Kotonoha
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         MATTN/Net-Kotonoha-0.08.tar.gz
    gitpan-cpan-author:       MATTN
    gitpan-cpan-maturity:     released

--- lib/Net/Kotonoha.pm
+++ lib/Net/Kotonoha.pm
@@ -9,7 +9,7 @@ use HTML::TreeBuilder::XPath;
 use HTML::Entities qw/decode_entities/;
 use Net::Kotonoha::Koto;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 sub new {
     my $class = shift;
